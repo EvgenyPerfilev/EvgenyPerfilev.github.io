177929ca9ce6eba3190517990f03aa9ed55e29f6
EvgenyPerfilev <127372740+EvgenyPerfilev@users.noreply.github.com>
2023-06-07 11:37:54 +1000

    Add files via upload

---
+++ Website/js/FAQ.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.faq details').forEach((item) =&gt; {
+  item.addEventListener('toggle', (event) =&gt; {
+    if (event.target.open) {
+      document.querySelectorAll('.faq details').forEach((otherItem) =&gt; {
+        if (otherItem !== event.target) {
+          otherItem.removeAttribute('open');
+        }
+      });
+    }
+  });
+});
